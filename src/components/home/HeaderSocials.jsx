1e4ecd3564e1904442adf3147b6c4d6491db2b90
George Ondigo <georgeondigo8@gmail.com>
2024-07-10 22:59:38 +0300

    second commit

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="https://www.instagram.com/ondigooo?igsh=MTBydDVhbzZvYW15MQ==" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="https://x.com/george__ondigo" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-x-twitter"></i>
+        </a>
+
+        <a href="https://github.com/Georgeondigo" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/george-ondigo/" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/avator-george1.png";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 const home = () => {
   return (
-    <div>home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">George Ondigo</h1>
+        <span className="home__education"> I'm a Software Engineer</span>
+        <HeaderSocials/>
+
+        <a href="#contact" className="btn">Hire Me</a>
+
+        <ScrollDown />
+      </div>
+    </section>
   )
 }
 
