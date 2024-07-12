f458689f3e8d46654f4965ac9afb85bac2008ed3
George Ondigo <georgeondigo8@gmail.com>
2024-07-13 01:31:38 +0300

    portfolio section

--- src/components/resume/data.jsx
+++ src/components/resume/data.jsx
@@ -1,53 +1,52 @@
 const Data = [
-    {
-      id: 1,
-      category: "education",
-      icon: "icon-graduation",
-      year: "2019 - present",
-      title: "Academic Degree",
-      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
-    },
-    {
-      id: 2,
-      category: "education",
-      icon: "icon-graduation",
-      year: "2013 - 2017",
-      title: "Bachelor's Degree",
-      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
-    },
-    {
-      id: 3,
-      category: "education",
-      icon: "icon-graduation",
-      year: "2009 - 2013",
-      title: "Honours Degree",
-      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
-    },
-    {
-      id: 4,
-      category: "experience",
-      icon: "icon-briefcase",
-      year: "2019 - present",
-      title: "Web Designer",
-      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
-    },
-    {
-      id: 5,
-      category: "experience",
-      icon: "icon-briefcase",
-      year: "2013 - 2017",
-      title: "Front-End Developer",
-      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
-    },
-    {
-      id: 6,
-      category: "experience",
-      icon: "icon-briefcase",
-      year: "2009 - 2013",
-      title: "Back-End Developer",
-      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
-    },
-  ];
-  
+  {
+    id: 1,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2022 - 2024",
+    title: "Diploma in Computer Science",
+    desc: "Completed coursework in Data Structures & Algorithms, Databases (SQL), Object Oriented Programming-Java/C++, Linux, Statistics, Operating Systems at Riara University.",
+  },
+  {
+    id: 2,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2024 - present",
+    title: "AWS re/Start Program",
+    desc: "Currently enrolled in the AWS re/Start program for cloud computing under Emobilis Institution of Technology and AWS Ajira program.",
+  },
+  {
+    id: 3,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "January 2024 - April 2024",
+    title: "Software Engineering Intern",
+    desc: "Contributed to backend and frontend development tasks at Flash Credit Africa, enhancing the customer experience for loan applications. Utilized C# .NET, Svelte, TypeScript, Tailwind CSS, Cosmos DB, and various Azure services.",
+  },
+  {
+    id: 4,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "March 2024 - April 2024",
+    title: "Web Development and Designing Intern",
+    desc: "Engaged in web development and design tasks at Oasis Infobyte, creating applications and implementing authentication systems using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+  },
+  {
+    id: 5,
+    category: "projects",
+    icon: "icon-project",
+    year: "2023",
+    title: "Art Gallery Web Application",
+    desc: "Developed a comprehensive web application for buying and selling artwork, featuring customer and admin portals, user authentication, payment integration, and artist requests. Built with React.js, Node.js, Express.js, and MongoDB.",
+  },
+  {
+    id: 6,
+    category: "projects",
+    icon: "icon-project",
+    year: "2023",
+    title: "Restaurant Food Ordering Website",
+    desc: "Created a website for online food ordering with features like menu display, user registration and authentication, order management, payment integration, and an admin dashboard. Built using React.js, Node.js, Express.js, and MongoDB.",
+  },
+];
 
-export default Data
+export default Data;
