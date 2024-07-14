31039460bfee21a0fae81fcac1fa31c563651e50
George Ondigo <georgeondigo8@gmail.com>
2024-07-14 21:09:05 +0300

    added the formspree email sender

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,41 +1,66 @@
 import React from 'react';
 import "./contacts.css";
 
-const contacts = () => {
+const Contacts = () => {
   return (
-   <section className="contact container section" id='contacts'>
-    <h2 className="section__title">Get In Touch</h2>
+    <section className="contact container section" id='contacts'>
+      <h2 className="section__title">Get In Touch</h2>
 
-    <div className="contact__container grid">
-      <div className="contact__info">
-        <h3 className="contact--title">Let's talk about everything!</h3>
-        <p className="contact__details">Don't like forms? Send me an email. 👋</p>
-      </div>
-
-      <form action="" className="contact__form">
-        <div className="contact__form-group">
-          <div className="contact__form-div">
-            <input type="text" className="contact__form-input" placeholder='Insert your name' />
-          </div>
-
-          <div className="contact__form-div">
-            <input type="email" className="contact__form-input" placeholder='Insert your email' />
-          </div>
+      <div className="contact__container grid">
+        <div className="contact__info">
+          <h3 className="contact--title">Let's talk about everything!</h3>
+          <p className="contact__details">Don't like forms? Send me an email. 👋</p>
         </div>
 
-        <div className="contact__form-div">
-            <input type="text" className="contact__form-input" placeholder='Insert your subject' />
+        <form action="https://formspree.io/f/mzzpzgnn" method="POST" className="contact__form">
+          <div className="contact__form-group">
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder='Insert your name'
+                name="name"
+                required
+              />
+            </div>
+
+            <div className="contact__form-div">
+              <input
+                type="email"
+                className="contact__form-input"
+                placeholder='Insert your email'
+                name="_replyto"
+                required
+              />
+            </div>
           </div>
 
-        <div className="contact__form-div contact__form-area" >
-          <textarea name="" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your message'></textarea>
-        </div> 
+          <div className="contact__form-div">
+            <input
+              type="text"
+              className="contact__form-input"
+              placeholder='Insert your subject'
+              name="subject"
+              required
+            />
+          </div>
 
-        <button className="btn">Send Message</button>
-      </form>
-    </div>
-   </section>
-  )
-}
+          <div className="contact__form-div contact__form-area">
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              className='contact__form-input'
+              placeholder='Write your message'
+              required
+            ></textarea>
+          </div>
 
-export default contacts
+          <button type="submit" className="btn">Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contacts;
