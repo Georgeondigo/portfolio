820c424ac369c251639db936d79786fb824a0220
George Ondigo <georgeondigo8@gmail.com>
2024-07-14 21:27:43 +0300

    added new logo

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./sidebar.css";
-import Logo from "../../assets/logo.svg";
+import Logo from "../../assets/george.png";
 
 const Sidebar = () => {
   const [toggle, showMenu] = useState(false);
