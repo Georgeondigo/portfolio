3dfdb8f7895a8273ed283fcdcbdd05ee25f95b76
George Ondigo <georgeondigo8@gmail.com>
2024-07-15 22:36:37 +0300

    added the cloud logo and also updated the pricing plan

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./services.css";
 import Image1 from '../../assets/service-1.svg';
 import Image2 from '../../assets/service-2.svg';
-import Image3 from '../../assets/service-2.svg';
+import Image3 from '../../assets/cloud.svg';
 
 const data = [
   {
